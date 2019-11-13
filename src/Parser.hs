ca4868b72736dc30a78f5f515511c9e7562f1618
konsumlamm <konsumlamm@gmail.com>
2019-11-14 00:17:42 +0100

    add typename, identifier parsers

--- src/Parser.hs
+++ src/Parser.hs
@@ -14,24 +14,56 @@ import Text.Parsec
 import Text.Parsec.Char
 import Text.Parsec.Text (Parser)
 
+data AST
+    = Identifier Text
+    | Typename Text
+
+
+uppercase :: String
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+lowercase :: String
+lowercase = "_abcdefghijklmnopqrstuvwxyz"
+
+numeric :: String
+numeric = "1234567890"
+
+alphanum :: String
+alphanum = lowercase ++ uppercase ++ numeric
+
+
 -- maybe we should check if String or Text is faster for HashSet?
 keywords :: HashSet Text
-keywords = Set.fromList ["let"]
+keywords = Set.fromList ["let", "sig"]
 
-parseUnit :: Text -> Either ParseError ()
-parseUnit text = parse unit "test" text
+parseUnit :: Text -> Either ParseError [()]
+parseUnit text = parse (spaces *> unit) "test" text
 
-unit :: Parser ()
-unit = eof
+unit :: Parser [()]
+unit = many definition
 
 -- let bindings
 definition :: Parser ()
 definition = eof  -- temp
 
-identifier :: Parser ()
-identifier = eof  -- temp
+typename :: Parser AST
+typename = lexeme $ do
+    a <- oneOf uppercase
+    b <- many (oneOf alphanum)
+    let name = T.pack (a : b)
+    if Set.member name keywords
+        then unexpected "keyword"
+        else Typename <$> pure name
+
+identifier :: Parser AST
+identifier = lexeme $ do
+    a <- oneOf lowercase
+    b <- many (oneOf alphanum)
+    let name = T.pack (a : b)
+    if Set.member name keywords
+        then unexpected "keyword"
+        else Identifier <$> pure name
 
 
--- parses the string and whitespace after it
-lexeme :: String -> Parser Text
-lexeme s = fmap T.pack (string s) <* spaces
+lexeme :: Parser a -> Parser a
+lexeme = (<* spaces)
